11dc41427c4ec3326d17b22ee51cfb73c6a1229b
yushengfan <yushengfan@3tcloud.com>
2018-01-20 13:35:40 +0800

    first commit

---
+++ src/Bean/TreeNode.java
@@ -0,0 +1,14 @@
+package Bean;
+
+/**
+ * Created by sendo on 2017/2/6.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
