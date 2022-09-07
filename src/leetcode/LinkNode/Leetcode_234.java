b01d03c12b502ca7935290362e8aa324bd58eaea
blancat <yu_shengfan@163.com>
2022-09-07 21:06:38 +0800

    2022-08-16

--- src/leetcode/LinkNode/Leetcode_234.java
+++ src/leetcode/LinkNode/Leetcode_234.java
@@ -9,10 +9,64 @@ public class Leetcode_234 {
 
     public static void main(String[] args) {
         Leetcode_234 leetcode_234 = new Leetcode_234();
-        ListNode listNode = CommonUtils.buildListNode(Arrays.asList(1, 2, 3, 4, 5));
-        ListNode reverseNode = leetcode_234.reverseNode(listNode);
-        CommonUtils.printListNode(reverseNode);
+        ListNode listNode = CommonUtils.buildListNode(Arrays.asList(1, 2, 3, 2, 1));
+        leetcode_234.isPalindrome2(listNode);
     }
+
+
+    public boolean isPalindrome2(ListNode head) {
+        // 快慢指针算法
+
+        ListNode left = head;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        ListNode right = reverse(slow);
+
+        while(right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = head.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+
+    }
+
+    private ListNode reverseNode(ListNode head) {
+
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+
+
+    }
+
+
 
     ListNode left;
     public void isPalindrome(ListNode head) {
@@ -30,18 +84,6 @@ public class Leetcode_234 {
     }
 
 
-    private ListNode reverseNode(ListNode head) {
 
-        ListNode pre = null, cur = head;
-        while (cur != null) {
-            ListNode temp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = temp;
-        }
-        return pre;
-
-
-    }
 
 }
